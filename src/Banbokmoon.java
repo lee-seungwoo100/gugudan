b473073aa0498cb55719e87a73d1cd5fadc9ee77
vugil <vugil@192.168.0.4>
2022-07-10 13:16:56 +0900

    initial commit

---
+++ src/Banbokmoon.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Banbokmoon {
+	public static void main(String[] args) {
+	// 6단 구구단
+//	Scanner scanner = new Scanner(System.in);
+//	int number = scanner.nextInt();
+//	
+//	int i = 1;
+//	while (i < 10) {
+//		System.out.println(number*i);
+//		i += 1;
+//	}
+
+	Scanner scanner = new Scanner(System.in);
+	int num = scanner.nextInt();
+	
+		
+	for(int j=1; j<10; j++) {
+		System.out.println(num*j);
+	}
+}
+	
+}
